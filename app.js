34e60b00be7b09fc0dc3f04ab853e5f4bea42d2d
sreen020 <sjoerdreen@gmail.com>
2021-03-09 21:47:30 +0100

    MVP

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const fetch = require("node-fetch");
+import express from "express";
 
 const app = express();
 const port = 3000;
+
+import { router } from "./routes/router.js";
 
 // static files
 app.use(express.static("public"));
@@ -11,47 +12,8 @@ app.use(express.static("public"));
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
+// Routes
+app.use("/", router);
 
-// Create a route for our overview page
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-
-app.get("/games", (req, res) => {
-  fetch("https://free-nba.p.rapidapi.com/games", {
-    headers: {
-      "x-rapidapi-key": "85d43d1b47msh7273a9c7d1d5c94p1b0a1cjsnbc483a52d1e3",
-      "x-rapidapi-host": "free-nba.p.rapidapi.com",
-      useQueryString: true,
-    },
-  })
-    .then((res) => res.json())
-    .then((json) =>
-      res.render("games.ejs", {
-        title: "Posts",
-        data: json,
-      })
-    );
-});
-
-app.get("/game/:id", (req, res) => {
-  fetch(`https://free-nba.p.rapidapi.com/games/${req.params.id}`, {
-    headers: {
-      "x-rapidapi-key": "85d43d1b47msh7273a9c7d1d5c94p1b0a1cjsnbc483a52d1e3",
-      "x-rapidapi-host": "free-nba.p.rapidapi.com",
-      useQueryString: true,
-    },
-  })
-    .then((res) => res.json())
-    .then((json) =>
-      res.render("spicificGame.ejs", {
-        title: "Posts",
-        data: json,
-      })
-    );
-});
-
+// Start server
 app.listen(port, () => console.log("Listening to port " + port));
---
+++ routes/router.js
@@ -0,0 +1,41 @@
+import express from "express";
+export const router = express.Router();
+import fetch from "node-fetch";
+
+router.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+router.get("/games", (req, res) => {
+  fetch("https://free-nba.p.rapidapi.com/games", {
+    headers: {
+      "x-rapidapi-key": "85d43d1b47msh7273a9c7d1d5c94p1b0a1cjsnbc483a52d1e3",
+      "x-rapidapi-host": "free-nba.p.rapidapi.com",
+      useQueryString: true,
+    },
+  })
+    .then((res) => res.json())
+    .then((json) =>
+      res.render("games.ejs", {
+        title: "Games",
+        data: json,
+      })
+    );
+});
+
+router.get("/games/:id", (req, res) => {
+  fetch(`https://free-nba.p.rapidapi.com/games/${req.params.id}`, {
+    headers: {
+      "x-rapidapi-key": "85d43d1b47msh7273a9c7d1d5c94p1b0a1cjsnbc483a52d1e3",
+      "x-rapidapi-host": "free-nba.p.rapidapi.com",
+      useQueryString: true,
+    },
+  })
+    .then((res) => res.json())
+    .then((json) =>
+      res.render("spicificGame.ejs", {
+        title: "Posts",
+        data: json,
+      })
+    );
+});
