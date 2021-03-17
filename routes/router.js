6474ab3194e8d2736028692dfc8908ed436bec50
sreen020 <sjoerdreen@gmail.com>
2021-03-17 10:06:33 +0100

    added a fetch module

--- routes/router.js
+++ routes/router.js
@@ -1,26 +1,14 @@
 import express from "express";
 export const router = express.Router();
 import fetch from "node-fetch";
+import fetchData from "./../scripts/api/fetchData.js";
 
 router.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
 router.get("/games", (req, res) => {
-  fetch("https://free-nba.p.rapidapi.com/games", {
-    headers: {
-      "x-rapidapi-key": "85d43d1b47msh7273a9c7d1d5c94p1b0a1cjsnbc483a52d1e3",
-      "x-rapidapi-host": "free-nba.p.rapidapi.com",
-      useQueryString: true,
-    },
-  })
-    .then((res) => res.json())
-    .then((json) =>
-      res.render("games.ejs", {
-        title: "Games",
-        data: json,
-      })
-    );
+  fetchData(req, res);
 });
 
 router.get("/games/:id", (req, res) => {
---
+++ scripts/api/fetchData.js
@@ -0,0 +1,18 @@
+import fetch from "node-fetch";
+
+export default function fetchData(req, res) {
+  fetch("https://free-nba.p.rapidapi.com/games", {
+    headers: {
+      "x-rapidapi-key": "85d43d1b47msh7273a9c7d1d5c94p1b0a1cjsnbc483a52d1e3",
+      "x-rapidapi-host": "free-nba.p.rapidapi.com",
+      useQueryString: true,
+    },
+  })
+    .then((res) => res.json())
+    .then((json) =>
+      res.render("games.ejs", {
+        title: "Games",
+        data: json,
+      })
+    );
+}
